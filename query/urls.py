ec0f9ff8c79949f9378eefd831cb6fadfa1d8967
WebMuhammadBR <81002797+WebMuhammadBR@users.noreply.github.com>
2026-03-13 15:39:41 +0500

    Implement working Django login page with static assets

--- query/urls.py
+++ query/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import farmer_report,home
+
+from .views import farmer_report, home, login_view
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", login_view, name="login"),
+    path("home/", home, name="home"),
     path("report/farmer/", farmer_report, name="farmer_report"),
 ]
