f00a336430a15c48b2e6ae4efd93ee205d8b8b2b
webmuhammadbr <raximovbobur300@gmail.com>
2026-03-23 14:19:26 +0500

    oldingi holatriga qaytdi

--- query/urls.py
+++ query/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
-
-from .views import farmer_report, home
+from .views import farmer_report,home
 
 urlpatterns = [
-    path("login/", auth_views.LoginView.as_view(template_name="query/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
     path("", home, name="home"),
     path("report/farmer/", farmer_report, name="farmer_report"),
 ]
--- query/views.py
+++ query/views.py
@@ -1,12 +1,12 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
-
 
 def home(request):
     return HttpResponse("Salom Bobur aka 👋")
 
 
-@login_required
+
+
 def farmer_report(request):
+
     return render(request, "query/farmer_report.html")
